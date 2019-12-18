cd6b3100bfa3cf9f926056a3e111faa852b70846
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-12-18 21:41:27 +0600

    UI

---
+++ Notes/Notes/DataAccess/NotesContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.DataAccess
+{
+    public class NotesContext
+    {
+    }
+}
---
+++ Notes/Notes/Domain/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Domain
+{
+    public class Note
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public DateTime? DeletedDate { get; set; }
+        public string TitleName { get; set; }
+        public string Text { get; set; }
+        public bool Is
+    }
+}
