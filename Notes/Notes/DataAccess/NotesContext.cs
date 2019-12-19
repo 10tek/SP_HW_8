078a9af749524f5f34d5eb23031c15d0ae0aedd0
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-12-19 16:42:41 +0600

    Добавил контекст

--- Notes/Notes/DataAccess/NotesContext.cs
+++ Notes/Notes/DataAccess/NotesContext.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Notes.DataAccess
 {
-    public class NotesContext
+    public class NotesContext : DbContext
     {
+        public NotesContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"DESKTOP-TALUG4B\SQLEXPRESS;Database=GameAd;Trusted_Connection=True;");
+        }
     }
 }
